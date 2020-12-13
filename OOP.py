a6ffbd870e3573728f2990096c931c9045422262
Yanis17971 <75942355+Yanis17971@users.noreply.github.com>
2020-12-13 19:06:23 +0300

    Add show

--- OOP.py
+++ OOP.py
@@ -5,19 +5,21 @@ class Blank:
         self.address=address
         print ("Объект создан")
 
+    def show(self):
+        print(self.name,self.phone,mike.address)
+
 
 mike=Blank("Михаил Булгаков","20327","Москва,Красная площадь")
 Vlad=Blank("Владимир Маяковский","38357","Москва,улица Ленина")
 Oleg=Blank("Олег","314324","Москва,улица Арбат")
 
-def print_contact():
-    print(mike.name,mike.phone,mike.address)
-    print(Vlad.name,Vlad.phone,Vlad.address)
-    print(Oleg.name,Oleg.phone,Oleg.address)
+
 
 
 
 Vlad.address="Санкт-Петербург,улица Ленина"
 
 
-print_contact()
+mike.show()
+Vlad.show()
+Oleg.show()
