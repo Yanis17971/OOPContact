8f6cc070d0a0c0d991e89d5d9e2ce92dbaccb790
Yanis17971 <75942355+Yanis17971@users.noreply.github.com>
2020-12-14 19:18:52 +0300

    +class and show

--- oop2.py
+++ oop2.py
@@ -18,7 +18,26 @@ class City(Point):
         self.name=name
         self.pop=pop
 
+    def show(self):
+        return (f"Город {self.name},Население {self.pop}")
+
+class Mountain(Point):
+    def __init__(self,x,y,name,height):
+        super().__init__(x,y)
+        self.name=name
+        self.height=height
+
+    def show(self):
+        return (f"Название горы {self.name},Высота {self.height}")
+        
+
+
 Moscow=City(55.7522200,37.6155600,"Москва",12615882)
 Chelyabinsk=City(55.154,61.4291,"Челябинск",1200703)
+Elbrus=Mountain(27.98791,86.98791,"Эльбрус",6794)
+Everest=Mountain(46.84931,53.84295,"Эверест",5899)
 
 print(Moscow.distance(Chelyabinsk))
+print(Elbrus.distance(Everest))
+print(Elbrus.show())
+print(Moscow.show())
